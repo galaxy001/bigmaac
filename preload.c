94f117dbd05bbf3351254fb346b75c04fe47d537
Yuuki Galaxy <galaxy001@gmail.com>
2025-01-16 17:09:49 +0800

    clang-formated.

--- preload.c
+++ preload.c
@@ -3,11 +3,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    if (argc<3) {
-        fprintf(stdout, "%s LIB.so exec ...\n",argv[0]);
-        return 0;
-    }
-    setenv("LD_PRELOAD", argv[1], 1);
-    execvp( argv[2], argv+2);
-    return 0;
+	if (argc < 3) {
+		fprintf(stdout, "%s LIB.so exec ...\n", argv[0]);
+		return 0;
+	}
+	setenv("LD_PRELOAD", argv[1], 1);
+	execvp(argv[2], argv + 2);
+	return 0;
 }
